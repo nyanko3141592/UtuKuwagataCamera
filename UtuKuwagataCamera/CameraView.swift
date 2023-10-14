ff39572450467cdf975c8f8f362c1fa33957c467
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2023-10-15 01:03:37 +0900

    コメント追加

--- UtuKuwagataCamera/CameraView.swift
+++ UtuKuwagataCamera/CameraView.swift
@@ -8,11 +8,19 @@
 import SwiftUI
 import UIKit
 
+// SwiftUIとUIKitをブリッジするカスタムビューを定義
 struct CameraView: UIViewControllerRepresentable {
+    
+    // 撮影した画像を保存するためのBindableなUIImage
     @Binding var image: UIImage?
+    
+    // カメラ上にオーバーレイする画像のBindableなUIImage
     @Binding var selectedImage: UIImage?
+    
+    // ImagePickerが表示されているかどうかを制御するBindableなBool
     @Binding var isImagePickerDisplayed: Bool
 
+    // CoordinatorはUIImagePickerControllerのデリゲートとして機能するクラス
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         let parent: CameraView
 
@@ -20,25 +28,30 @@ struct CameraView: UIViewControllerRepresentable {
             self.parent = parent
         }
 
+        // 画像が選択または撮影されたときに呼ばれるデリゲートメソッド
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
             if let takenImage = info[.originalImage] as? UIImage {
                 parent.image = takenImage
                 
+                // 撮影した画像をカメラロールに保存
                 UIImageWriteToSavedPhotosAlbum(takenImage, nil, nil, nil)
             }
 
             parent.isImagePickerDisplayed = false
         }
 
+        // ImagePickerがキャンセルされたときに呼ばれるデリゲートメソッド
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             parent.isImagePickerDisplayed = false
         }
     }
 
+    // SwiftUIからCoordinatorを作成するためのメソッド
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
 
+    // SwiftUIから初めてUIViewControllerが要求されるときに呼ばれるメソッド
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
@@ -47,11 +60,13 @@ struct CameraView: UIViewControllerRepresentable {
         return picker
     }
 
+    // SwiftUIがUIViewControllerを更新する必要があるときに呼ばれるメソッド
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
         uiViewController.cameraOverlayView = UIImageView(image: selectedImage?.withAlpha(0.5))
     }
 }
 
+// UIImageを拡張して透明度を調整するメソッドを追加
 extension UIImage {
     func withAlpha(_ alpha: CGFloat) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(size, false, scale)
@@ -61,4 +76,3 @@ extension UIImage {
         return newImage
     }
 }
-
--- UtuKuwagataCamera/ContentView.swift
+++ UtuKuwagataCamera/ContentView.swift
@@ -6,16 +6,20 @@
 //
 import SwiftUI
 
-import SwiftUI
-
+// 主要なビューを定義
 struct ContentView: View {
+    // 撮影または選択された画像を保持するためのState
     @State private var image: UIImage?
+    // カメラオーバーレイとして使用する画像を保持するためのState
     @State private var selectedImage: UIImage?
+    // ImagePicker表示のトグル
     @State private var isImagePickerDisplayed: Bool = false
+    // CameraView表示のトグル
     @State private var isCameraDisplayed: Bool = false
 
     var body: some View {
         VStack(spacing: 20) {
+            // 選択された画像のプレビュー
             if let selectedImage = selectedImage {
                 Image(uiImage: selectedImage)
                     .resizable()
@@ -25,28 +29,35 @@ struct ContentView: View {
                 Text("画像が選択されていません")
             }
 
+            // カメラロールからの画像選択ボタン
             Button("カメラロールから画像を選択") {
                 isImagePickerDisplayed = true
             }
 
+            // カメラを起動するボタン
             Button("カメラを開く") {
                 isCameraDisplayed = true
             }
         }
+        // ImagePickerの表示
         .sheet(isPresented: $isImagePickerDisplayed) {
             ImagePicker(image: $selectedImage, sourceType: .photoLibrary)
         }
+        // CameraViewの表示
         .sheet(isPresented: $isCameraDisplayed) {
             CameraView(image: $image, selectedImage: $selectedImage, isImagePickerDisplayed: $isCameraDisplayed)
         }
     }
 }
 
-
+// カメラロールから画像を選択するためのビューを定義
 struct ImagePicker: UIViewControllerRepresentable {
+    // 選択された画像を保持するためのBindableなUIImage
     @Binding var image: UIImage?
+    // 画像ソースのタイプ（カメラ、フォトライブラリなど）
     var sourceType: UIImagePickerController.SourceType
-    
+
+    // UIImagePickerControllerのデリゲートとして機能するCoordinator
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         let parent: ImagePicker
 
@@ -54,6 +65,7 @@ struct ImagePicker: UIViewControllerRepresentable {
             self.parent = parent
         }
 
+        // 画像が選択されたときに呼ばれるデリゲートメソッド
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
             if let uiImage = info[.originalImage] as? UIImage {
                 parent.image = uiImage
@@ -61,23 +73,27 @@ struct ImagePicker: UIViewControllerRepresentable {
 
             picker.dismiss(animated: true)
         }
-        
+
+        // ImagePickerがキャンセルされたときに呼ばれるデリゲートメソッド
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             picker.dismiss(animated: true)
         }
     }
 
+    // SwiftUIからCoordinatorを作成するためのメソッド
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-    
+
+    // SwiftUIから初めてUIViewControllerが要求されるときに呼ばれるメソッド
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
         picker.sourceType = sourceType
         return picker
     }
-    
+
+    // SwiftUIがUIViewControllerを更新する必要があるときに呼ばれるメソッド（ここでは何もしない）
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
     }
 }
