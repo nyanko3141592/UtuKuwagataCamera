a9f5523dee468fc0c72d111184472fa0fa660e2c
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2023-10-14 23:37:30 +0900

    Initial Commit

---
+++ UtuKuwagataCamera/UtuKuwagataCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  UtuKuwagataCameraApp.swift
+//  UtuKuwagataCamera
+//
+//  Created by 高橋直希 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct UtuKuwagataCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
